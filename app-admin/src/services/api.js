9d2e8b61641a6e98643e4766193462efa4ffdca4
stevesoft-dark <pisciniaustrini@gmail.com>
2026-01-23 16:34:52 +0100

    reco

--- app-admin/src/services/api.js
+++ app-admin/src/services/api.js
@@ -1,5 +1,5 @@
 // src/services/api.js
-const API_BASE_URL = 'http://localhost:3001/api';
+const API_BASE_URL = 'https://application-global-express-delivery-back.onrender.com/api';
 
 export const apiRequest = async (endpoint, options = {}) => {
   const token = localStorage.getItem('token');
